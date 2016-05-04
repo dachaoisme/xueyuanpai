95b29bf1e73089aaa22dd2fb88d5931801a42c9c
lezyo-org <lidachao@lidachaodemacbook-pro.local>
2016-05-04 09:21:59 +0800

    Initial Commit

---
+++ xueyuanpai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xueyuanpai
+//
+//  Created by lidachao on 16/5/4.
+//  Copyright © 2016年 lidachao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
