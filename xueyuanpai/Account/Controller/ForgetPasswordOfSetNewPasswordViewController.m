5fe437a0f4bda475de3df8f94974cc495e4e1d4c
dachaoisme <1522773761@qq.com>
2016-05-23 05:21:12 +0800

    搭建忘记密码界面和处理相关逻辑

---
+++ xueyuanpai/Account/Controller/ForgetPasswordOfSetNewPasswordViewController.m
@@ -0,0 +1,109 @@
+//
+//  ForgetPasswordOfSetNewPasswordViewController.m
+//  xueyuanpai
+//
+//  Created by lidachao on 16/5/23.
+//  Copyright © 2016年 lidachao. All rights reserved.
+//
+
+#import "ForgetPasswordOfSetNewPasswordViewController.h"
+
+@interface ForgetPasswordOfSetNewPasswordViewController ()<UITextFieldDelegate>
+{
+    UITextField *inputPasswordTextField;
+    UIButton    *sureBtn;
+}
+@end
+
+@implementation ForgetPasswordOfSetNewPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [CommonUtils colorWithHex:@"e5e5e5"];
+    [self setContentView];
+    
+    
+}
+-(void)setContentView
+{
+    float space = 16;
+    float width = SCREEN_WIDTH -2*space;
+    float height = 48;
+    
+    UIView * backgroundView = [[UIView alloc]initWithFrame:CGRectMake(0, space+NAV_TOP_HEIGHT, SCREEN_WIDTH, height)];
+    backgroundView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:backgroundView];
+    
+    //请输入密码
+    inputPasswordTextField = [[UITextField alloc]initWithFrame:CGRectMake(space, 0, width, height)];
+    inputPasswordTextField.tag = 2;
+    inputPasswordTextField.delegate = self;
+    [inputPasswordTextField setBackgroundColor:[CommonUtils colorWithHex:@"ffffff"]];
+    inputPasswordTextField.textAlignment = NSTextAlignmentLeft;
+    inputPasswordTextField.borderStyle = UITextBorderStyleNone;
+    inputPasswordTextField.placeholder = @"请设置新密码";
+    inputPasswordTextField.adjustsFontSizeToFitWidth = YES;
+    inputPasswordTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [backgroundView addSubview:inputPasswordTextField];
+    
+    sureBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    sureBtn.layer.cornerRadius = 3.0;
+    [sureBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [sureBtn setBackgroundColor:[CommonUtils colorWithHex:@"00beaf"]];
+    [sureBtn setFrame:CGRectMake(space, CGRectGetMaxY(backgroundView.frame)+space, width, height)];
+    [sureBtn setContentMode:UIViewContentModeCenter];
+    [sureBtn addTarget:self action:@selector(sure:) forControlEvents:UIControlEventTouchUpInside];
+    [sureBtn setTitle:@"确定" forState:UIControlStateNormal];
+    sureBtn.titleLabel.font = [UIFont systemFontOfSize:10];
+    [self.view addSubview:sureBtn];
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)sure:(UIButton *)sender
+{
+    if (inputPasswordTextField.text.length<=0 ) {
+        [CommonUtils showToastWithStr:@"密码不能为空"];
+        return;
+    }
+    /*
+     user_id int    必需    用户序号
+     passwd   string    必需    新密码
+     */
+    //校验验证码
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setObject:self.userId forKey:@"user_id"];
+    [dic setObject:inputPasswordTextField.text forKey:@"passwd"];
+    [[HttpClient sharedInstance]changePasswordWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode == ResponseCodeSuccess) {
+            NSString * userId = [[model.responseCommonDic objectForKey:@"data"] objectForKey:@"user_id"];
+            //若成功，应该是返回主页面，并且是已经登录状态
+            [[UserAccountManager sharedInstance]saveUserAccountWithUserId:self.userId withPhoneNum:self.telephoneNum withPassword:inputPasswordTextField.text];
+        }else{
+            //验证失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        
+    }];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ xueyuanpai/Account/Controller/ForgetPasswordViewController.m
@@ -0,0 +1,211 @@
+//
+//  ForgetPasswordViewController.m
+//  xueyuanpai
+//
+//  Created by lidachao on 16/5/23.
+//  Copyright © 2016年 lidachao. All rights reserved.
+//
+
+#import "ForgetPasswordViewController.h"
+#import "ForgetPasswordOfSetNewPasswordViewController.h"
+@interface ForgetPasswordViewController ()<UITextFieldDelegate>
+{
+    UITextField *phoneTextField;
+    UIButton    *sendMessageBtn;
+    UITextField *checkingMessageTextField;
+    UIButton    *sureBtn;
+    
+    NSInteger     timerCount;
+    NSTimer     *sendTimer;
+    
+}
+@end
+
+@implementation ForgetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self setTitle:@"忘记密码"];
+    self.view.backgroundColor = [CommonUtils colorWithHex:@"e5e5e5"];
+    [self setContentView];
+}
+-(void)setContentView
+{
+    float space = 16;
+    float width = SCREEN_WIDTH -2*space;
+    float height = 48;
+    
+    UIView * backgroundView = [[UIView alloc]initWithFrame:CGRectMake(0, space+NAV_TOP_HEIGHT, SCREEN_WIDTH, height*2)];
+    backgroundView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:backgroundView];
+    
+    //请输入手机号
+    phoneTextField = [[UITextField alloc]initWithFrame:CGRectMake(space, 0, width, height)];
+    phoneTextField.tag = 2;
+    phoneTextField.delegate = self;
+    [phoneTextField setBackgroundColor:[CommonUtils colorWithHex:@"ffffff"]];
+    phoneTextField.textAlignment = NSTextAlignmentLeft;
+    phoneTextField.borderStyle   = UITextBorderStyleNone;
+    phoneTextField.placeholder   = @"请输入手机号";
+    //myTextField.clearsOnBeginEditing = YES;//设置为YES当用点触文本字段时，字段内容会被清除
+    phoneTextField.adjustsFontSizeToFitWidth = YES;
+    phoneTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    phoneTextField.leftViewMode    = UITextFieldViewModeAlways;
+    phoneTextField.rightViewMode   = UITextFieldViewModeAlways;
+    [backgroundView addSubview:phoneTextField];
+    
+    sendMessageBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    sendMessageBtn.layer.cornerRadius = 3.0;
+    [sendMessageBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [sendMessageBtn setBackgroundColor:[CommonUtils colorWithHex:@"00beaf"]];
+    [sendMessageBtn setFrame:CGRectMake(0, 0, 80, 30)];
+    [sendMessageBtn setContentMode:UIViewContentModeCenter];
+    [sendMessageBtn addTarget:self action:@selector(sendmessage:) forControlEvents:UIControlEventTouchUpInside];
+    [sendMessageBtn setTitle:@"发送验证码" forState:UIControlStateNormal];
+    sendMessageBtn.titleLabel.font = [UIFont systemFontOfSize:10];
+    phoneTextField.rightView = sendMessageBtn;
+ 
+    //请输入验证码
+    checkingMessageTextField = [[UITextField alloc]initWithFrame:CGRectMake(space, CGRectGetMaxY(phoneTextField.frame), width, height)];
+    checkingMessageTextField.tag = 2;
+    checkingMessageTextField.delegate = self;
+    [checkingMessageTextField setBackgroundColor:[CommonUtils colorWithHex:@"ffffff"]];
+    checkingMessageTextField.textAlignment = NSTextAlignmentLeft;
+    checkingMessageTextField.borderStyle = UITextBorderStyleNone;
+    checkingMessageTextField.placeholder = @"请输入验证码";
+    checkingMessageTextField.adjustsFontSizeToFitWidth = YES;
+    checkingMessageTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [backgroundView addSubview:checkingMessageTextField];
+    
+    
+    float lineHeight = 0.5;
+    [UIFactory showLineInView:backgroundView color:@"999999" rect:CGRectMake(space, CGRectGetMaxY(phoneTextField.frame), width+space,lineHeight)];
+    
+    sureBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    sureBtn.layer.cornerRadius = 3.0;
+    [sureBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [sureBtn setBackgroundColor:[CommonUtils colorWithHex:@"00beaf"]];
+    [sureBtn setFrame:CGRectMake(space, CGRectGetMaxY(backgroundView.frame)+space, width, height)];
+    [sureBtn setContentMode:UIViewContentModeCenter];
+    [sureBtn addTarget:self action:@selector(sure:) forControlEvents:UIControlEventTouchUpInside];
+    [sureBtn setTitle:@"确定" forState:UIControlStateNormal];
+    sureBtn.titleLabel.font = [UIFont systemFontOfSize:10];
+    [self.view addSubview:sureBtn];
+}
+
+-(void)sendmessage:(UIButton *)sender
+{
+    
+    NSString * phoneNum = phoneTextField.text;
+    if (!(phoneNum.length==11 && [CommonUtils checkPhoneNumIsAvailableWithPhoneNumString:phoneNum])) {
+        [CommonUtils showToastWithStr:@"请输入有效手机号"];
+        return;
+    }
+    
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    [params setObject:phoneNum forKey:@"mobile"];
+    [[HttpClient sharedInstance]registerOfSendMessageWithParams:params withSuccessBlock:^(HttpResponseCodeModel *model) {
+        
+        if (model.responseCode == ResponseCodeSuccess) {
+            [CommonUtils showToastWithStr:@"发送成功"];
+            //请求成功了，才改变发送按钮的倒计时
+            [self startTimer];
+        }else{
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+        
+    } withFaileBlock:^(NSError *error) {
+        
+        
+    }];
+    
+    
+}
+
+-(void)sure:(UIButton *)sender
+{
+    //验证码和手机号验证成功
+    if (phoneTextField.text.length<=0 || ![CommonUtils checkPhoneNumIsAvailableWithPhoneNumString:phoneTextField.text]) {
+        [CommonUtils showToastWithStr:@"请输入有效手机号"];
+        return;
+    }
+    if (checkingMessageTextField.text.length<=0 ||![CommonUtils checkIsNum:checkingMessageTextField.text]) {
+        [CommonUtils showToastWithStr:@"请输入有效验证码"];
+        return;
+    }
+    /*
+     mobile  string    必需    手机号码
+     captcha   string    必需    验证码
+     
+     */
+    //校验验证码
+    NSMutableDictionary * dic = [NSMutableDictionary dictionary];
+    [dic setObject:phoneTextField.text forKey:@"mobile"];
+    [dic setObject:checkingMessageTextField.text forKey:@"captcha"];
+    [[HttpClient sharedInstance]validateTelephoneAndSecurityCodeWithParams:dic withSuccessBlock:^(HttpResponseCodeModel *model) {
+        if (model.responseCode == ResponseCodeSuccess) {
+            NSString * userId = [[model.responseCommonDic objectForKey:@"data"] objectForKey:@"user_id"];
+            //验证码和手机号验证成功
+            ForgetPasswordOfSetNewPasswordViewController * forgetPasswordOfSetNewPasswordVC = [[ForgetPasswordOfSetNewPasswordViewController alloc] init];
+            forgetPasswordOfSetNewPasswordVC.userId = userId;
+            forgetPasswordOfSetNewPasswordVC.telephoneNum = phoneTextField.text;
+            [self.navigationController pushViewController:forgetPasswordOfSetNewPasswordVC animated:YES];
+            
+        }else{
+            //验证失败
+            [CommonUtils showToastWithStr:model.responseMsg];
+        }
+    } withFaileBlock:^(NSError *error) {
+        
+    }];
+    
+}
+#pragma mark - 倒计时相关
+-(void)startTimer
+{
+    timerCount = 60;
+    sendTimer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                 target:self
+                                               selector:@selector(countTimer:)
+                                               userInfo:nil
+                                                repeats:YES];
+    
+    sendMessageBtn.userInteractionEnabled = NO;
+    [sendMessageBtn setTitle:@"60" forState:UIControlStateNormal];
+}
+-(void)countTimer:(NSTimer*)timer
+{
+    timerCount = timerCount-1;
+    NSString * btnTittle = [NSString stringWithFormat:@"%lds",(long)timerCount];
+    [sendMessageBtn setTitle:btnTittle forState:UIControlStateNormal];
+    if (timerCount <= 0) {
+        [self killTimer];
+    }
+}
+- (void)killTimer{
+    sendMessageBtn.userInteractionEnabled = YES;
+    [sendMessageBtn setTitle:@"发送验证码" forState:UIControlStateNormal];
+    
+    if(sendTimer)
+    {
+        [sendTimer invalidate];
+        sendTimer = nil;
+    }
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
