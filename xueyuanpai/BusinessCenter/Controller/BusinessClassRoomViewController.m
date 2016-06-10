141fd685ce45a14327e594f863c73fb2228fea4b
sunshineyuan <1390936239@qq.com>
2016-06-10 11:26:13 +0800

    自适应cell的bug修改

--- xueyuanpai/BusinessCenter/Controller/BusinessClassRoomViewController.m
+++ xueyuanpai/BusinessCenter/Controller/BusinessClassRoomViewController.m
@@ -67,7 +67,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    return 100;
+    return 115;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
