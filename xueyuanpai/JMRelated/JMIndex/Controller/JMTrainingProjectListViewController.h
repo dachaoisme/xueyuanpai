51b10da19619244d6dbb78cc5f56f35612a4cfd2
sunshineyuan <1390936239@qq.com>
2017-04-12 19:57:02 +0800

    实训项目列表的界面展示完毕

---
+++ xueyuanpai/JMRelated/JMIndex/Controller/JMTrainingProjectListViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMTrainingProjectListViewController.h
+//  xueyuanpai
+//
+//  Created by 王园园 on 2017/4/12.
+//  Copyright © 2017年 lidachao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface JMTrainingProjectListViewController : BaseViewController
+
+@end
---
+++ xueyuanpai/JMRelated/JMIndex/Controller/JMTrainingProjectListViewController.m
@@ -0,0 +1,97 @@
+//
+//  JMTrainingProjectListViewController.m
+//  xueyuanpai
+//
+//  Created by 王园园 on 2017/4/12.
+//  Copyright © 2017年 lidachao. All rights reserved.
+//
+
+#import "JMTrainingProjectListViewController.h"
+
+#import "JMHomePageThreeTypeTableViewCell.h"
+
+@interface JMTrainingProjectListViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong)UITableView *tableView;
+
+@end
+
+@implementation JMTrainingProjectListViewController
+
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [super viewWillAppear:animated];
+    
+    [self theTabBarHidden:YES];
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    self.title = @"实训项目列表";
+    
+    [self createLeftBackNavBtn];
+    
+    [self createTableView];
+}
+
+#pragma mark - 创建tableView列表视图
+- (void)createTableView{
+    
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0,0, SCREEN_WIDTH, SCREEN_HEIGHT) style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+    [_tableView registerClass:[JMHomePageThreeTypeTableViewCell class] forCellReuseIdentifier:@"JMHomePageThreeTypeTableViewCell"];
+    
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+   
+    return 3;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    JMHomePageThreeTypeTableViewCell *threeCell = [tableView dequeueReusableCellWithIdentifier:@"JMHomePageThreeTypeTableViewCell"];
+    
+    
+    return threeCell;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 100;
+
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    
+    return 0.01;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
