24ce1454531fd67e16acb9da793dc025b35d56fd
dachao li <1522773761@qq.com>
2017-04-27 15:02:17 +0800

    创建下单成功的页面

---
+++ xueyuanpai/JMRelated/JMKuaiDiYuan/Controller/JMSuccessSendExpressViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMSuccessSendExpressViewController.h
+//  xueyuanpai
+//
+//  Created by dachao li on 2017/4/27.
+//  Copyright © 2017年 lidachao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface JMSuccessSendExpressViewController : BaseViewController
+
+@end
