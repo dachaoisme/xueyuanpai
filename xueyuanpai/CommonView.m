f764bc8f5d3f9a95dfaa9eac867656a2b8a70836
dachao li <1522773761@qq.com>
2017-04-27 15:57:44 +0800

    添加空数据页面类

---
+++ xueyuanpai/CommonView.m
@@ -0,0 +1,13 @@
+//
+//  CommonView.m
+//  xueyuanpai
+//
+//  Created by dachao li on 2017/4/27.
+//  Copyright © 2017年 lidachao. All rights reserved.
+//
+
+#import "CommonView.h"
+
+@implementation CommonView
+
+@end
