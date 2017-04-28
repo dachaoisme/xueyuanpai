c436d40d640d623f0b284ecbc2ef3536e7cc4c82
dachao li <1522773761@qq.com>
2017-04-28 08:38:46 +0800

    调整个人中心页面实训列表界面数据

--- xueyuanpai/JMRelated/JMMine/Controller/JMMineProjectListViewController.m
+++ xueyuanpai/JMRelated/JMMine/Controller/JMMineProjectListViewController.m
@@ -72,7 +72,7 @@
         
         for (int i=0; i<listArray.count; i++) {
             NSDictionary *tempDic = [listArray objectAtIndex:i];
-            JMMineTrainCommonModel *model = [JMMineTrainCommonModel yy_modelWithDictionary:tempDic];
+            JMTrainProjectModel *model = [JMTrainProjectModel yy_modelWithDictionary:tempDic];
             [dataArray addObject:model];
             
         }
@@ -114,14 +114,14 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     JMHomePageThreeTypeTableViewCell *threeCell = [tableView dequeueReusableCellWithIdentifier:@"JMHomePageThreeTypeTableViewCell"];
-    JMMineTrainCommonModel * model = [dataArray objectAtIndex:indexPath.row];
-    [threeCell.showImageView sd_setImageWithURL:[NSURL URLWithString:model.icon] placeholderImage:[UIImage imageNamed:@"placeHoder"]];
-    threeCell.titleLabel.text = model.name;
-    threeCell.subtitleLabel.text = model.job;
+    JMTrainProjectModel * model = [dataArray objectAtIndex:indexPath.row];
+    [threeCell.showImageView sd_setImageWithURL:[NSURL URLWithString:model.thumbUrl] placeholderImage:[UIImage imageNamed:@"placeHoder"]];
+    threeCell.titleLabel.text = model.title;
+    threeCell.subtitleLabel.text = model.trainProjectDescription;
     [threeCell.locationBtn setTitle:model.colllege_name forState:UIControlStateNormal];
     if ([model.status integerValue]==1) {
         ///正在招募
-        threeCell.peopleNumberLabel.text = [NSString stringWithFormat:@"已招募%@",model.count_mark];
+        threeCell.peopleNumberLabel.text = [NSString stringWithFormat:@"已招募%@",model.recruitment_number ];
     }else{
         ///一结束
         threeCell.peopleNumberLabel.text = @"已结束";
@@ -144,7 +144,7 @@
 //点击跳转详情视图
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    JMMineTrainCommonModel * model = [dataArray objectAtIndex:indexPath.row];
+    JMTrainProjectModel * model = [dataArray objectAtIndex:indexPath.row];
     if ([model.status intValue]==1) {
         ///正在招募
         //未结束的项目的详情
@@ -156,16 +156,16 @@
          
          */
         JMHomePageViewTrainingProjectDetailController *detailVC = [[JMHomePageViewTrainingProjectDetailController alloc] init];
-        detailVC.title = model.name;
-        detailVC.trainProjectId = model.entity_id;
+        detailVC.title = model.title;
+        detailVC.trainProjectId = model.trainProjectId;
         detailVC.count_like = model.count_like;
-        detailVC.recruitment_number = model.count_mark;
+        detailVC.recruitment_number = model.recruitment_number;
         detailVC.count_comment = model.count_comment;
         [self.navigationController pushViewController:detailVC animated:YES];
     }else{
         //已结束的项目的详情
         JMHomePageEndProjectDetailViewController *endProject = [[JMHomePageEndProjectDetailViewController alloc] init];
-        endProject.trainProjectId = model.entity_id;
+        endProject.trainProjectId = model.trainProjectId;
         [self.navigationController pushViewController:endProject animated:YES];
     }
     
