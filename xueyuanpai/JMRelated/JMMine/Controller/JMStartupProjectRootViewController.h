54d25325c9b2f881956640eceb3214e26f209eda
dachao li <1522773761@qq.com>
2017-08-06 15:15:08 +0800

    添加我的收藏的相关文件

---
+++ xueyuanpai/JMRelated/JMMine/Controller/JMStartupProjectRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMStartupProjectRootViewController.h
+//  xueyuanpai
+//
+//  Created by dachao li on 2017/8/6.
+//  Copyright © 2017年 lidachao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface JMStartupProjectRootViewController : BaseViewController
+
+@end
